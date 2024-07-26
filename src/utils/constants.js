02459cf5514b1d0ba781f39c79a9d090944ce2cb
fredarex <fredobi200@gmail.com>
2024-07-26 08:00:01 +0100

    worked on deployment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,9 @@
 
 import nonFound from '../../public/images/nonfound.png';
-import newuser from '../../public/images/newuser.svg'
+import newuser from '../../public/images/newuser.svg';
+import person from '../../public/images/person.svg';
 export {
     nonFound,
-    newuser
+    newuser,
+    person
 }
